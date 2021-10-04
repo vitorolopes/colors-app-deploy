5f258d69597a1b64d5fad3ef88c7c93fbaa1cd3f
vitor lopes <lopesvitor959@gmail.com>
2021-10-04 15:52:51 +0100

    Added a <Slider/> from [rc-slider] library in <Palette>. The slider allows the user to choose  between different palette shades (lightest shades[100]) or (darkest shades[900]). Restyled <Palette>, Slider.

--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import ColorBox from './ColorBox';
-import './Palette.css';        
+import './Palette.css';     
+
 
 class Palette extends Component {
-    render() {   
-        const colorBoxes = this.props.palette.colors[800].map( color => (
+    constructor(props){
+        super(props);
+        this.state = { level: 100  };
+        this.changeLevel = this.changeLevel.bind(this);
+    }
+
+    changeLevel(newLevel){
+        // console.log("level changed")
+        this.setState( { level: newLevel})
+    }
+
+
+    render() { 
+        const {colors}=this.props.palette;
+        const {level}=this.state;
+        
+        const colorBoxes = colors[level].map( color => (
             <ColorBox background={color.hex} name={color.name} key={color.name} />
         ))
         return(
             <div className="Palette">
+                <div className="slider">
+                    <Slider
+                        min={100}
+                        max={900}
+                        step={100}
+                        defaultValue={level}
+                        onAfterChange={this.changeLevel}
+                    />
+                </div>
+
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>             
