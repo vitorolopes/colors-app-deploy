5903497f17457bf848055f423e291051695321f7
vitor lopes <lopesvitor959@gmail.com>
2021-10-10 15:38:17 +0100

    <ColorBox>. Dynamic Text Color with luminance method from [Chroma-js] library.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
 import {Link} from 'react-router-dom';
+
+import chroma from 'chroma-js';
 
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './ColorBox.css';
@@ -24,27 +25,36 @@ class ColorBox extends Component {
     render() {
         const {background, name, moreURL, showLink} = this.props;
         const {copied} = this.state;
+        // console.log(chroma(background).luminance()) --> This gives us a bunch
+        // of values (one for each color being rendered). If the color is dark
+        // the value is low and the opposite if the color is bright.
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isBrightColor = chroma(background).luminance() >= 0.7;
+
 
         return(
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div className="ColorBox" style={{background: background}}>
 
                     <div className={`copy-overlay ${copied && "show"}`} style={{background: background}}> </div>
+                    
                     <div className={`msg-overlay ${copied && "show"}`} >
                         <h1>Copied!</h1>
-                        <p>{background}</p>
+                        <p className={isBrightColor ? "dark-text" : null}>
+                                {background}
+                        </p>
                     </div>
                    
                     <div className="copy-container"> 
                         <div className="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor ? "bright-text" : null}>{name}</span>
                         </div>
-                        <button className="copy-button">Copy</button>   
+                        <button className={`copy-button ${isBrightColor && "dark-text"}`}>Copy</button>   
                     </div>
 
                     {showLink && 
                         <Link to={moreURL}>
-                            <span className="see-more">More</span>
+                            <span className={`see-more ${isBrightColor && "dark-text"}`}>More</span>
                         </Link>
                     }
                     
